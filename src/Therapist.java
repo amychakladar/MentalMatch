3d9e2371cee349440c0a1ea255fe9b2d8300ae6a
Amy <amritachakladar@berkeley.edu>
2023-03-03 18:17:23 -0800

    building a csv for data as opposed to keeping it in the main func

--- src/Therapist.java
+++ src/Therapist.java
@@ -1,3 +1,12 @@
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvValidationException;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 /** This class represents therapists with their specialties.
  * Written by @amychakladar*/
@@ -29,5 +38,31 @@ public class Therapist {
 
     public int getAge() {
         return age;
+    }
+
+    public static List<Therapist> readTherapists() {
+        List<Therapist> therapists = new ArrayList<>();
+
+        try {
+            InputStream inputStream = new FileInputStream("data/therapist.csv");
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+            CSVReader csvReader = new CSVReader(inputStreamReader);
+
+            String[] nextLine;
+            while ((nextLine = csvReader.readNext()) != null) {
+                String name = nextLine[0];
+                String specialty = nextLine[1];
+                String gender = nextLine[2];
+                int age = Integer.parseInt(nextLine[3]);
+                Therapist therapist = new Therapist(name, specialty, gender, age);
+                therapists.add(therapist);
+            }
+
+            csvReader.close();
+        } catch (IOException | CsvValidationException e) {
+            e.printStackTrace();
+        }
+
+        return therapists;
     }
 }
